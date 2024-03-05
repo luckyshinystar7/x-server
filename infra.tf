949ec6535edc63b0711a1403ea262e156feffba1
luckyshinystar7 <luckyshinystar7@gmail.com>
2024-03-05 16:19:14 +0100

    adding untagged images handle to infra

--- infra.tf
+++ infra.tf
@@ -99,6 +99,27 @@ resource "aws_ecr_repository" "my_ecr_repo" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "my_ecr_repo_policy" {
+  repository = aws_ecr_repository.my_ecr_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description = "Expire untagged images"
+        selection = {
+          tagStatus = "untagged"
+          countType = "imageCountMoreThan"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 output "ecr_repository_url" {
   value = aws_ecr_repository.my_ecr_repo.repository_url
 }
