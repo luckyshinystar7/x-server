dfeaa5dfe3afd5c343094e9a9eaf924f3554a428
luckyshinystar7 <luckyshinystar7@gmail.com>
2024-03-05 22:01:21 +0100

    little main.tf adjustments

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,8 @@ module "database" {
   vpc_id = module.networking.vpc_id
   db_subnet_group_name = module.networking.db_subnet_group_name # Assuming you have this output
   db_security_group_ids = [module.security.rds_sg_id]
-  database_username = "example_username" # Replace with actual username or variable
-  database_password = "example_password" # Replace with actual password or variable
+  database_username = "username" # Replace with actual username or variable
+  database_password = "password" # Replace with actual password or variable
 }
 
 module "networking" {
@@ -51,7 +51,7 @@ module "lambda" {
   database_username     = module.database.db_instance_username
   database_password     = module.database.db_instance_password
   database_address      = module.database.db_instance_address
-  database_name         = "specify_your_database_name_here"  # Assuming you have this defined elsewhere
+  database_name         = "twitter_db"  # Assuming you have this defined elsewhere
   lambda_subnet_id      = [module.networking.lambda_subnet_id]  # Ensure this is a list
   lambda_security_group_id = [module.security.lambda_sg_id]  # Ensure this is a list
   jwt_secret            = "846368bb86f674f8d5d706667ddbb003"  # Or source this from a secure location
@@ -59,4 +59,3 @@ module "lambda" {
   refresh_token_duration_minutes = "60"
   environment           = "PRODUCTION"
 }
-
